72cfcd479199139a0df13bfde17f3629d1f0515e
saisuvanth <saisuvanth@gmail.com>
2022-08-02 16:43:07 +0530

    cc

--- server/src/middlewares/user.ts
+++ server/src/middlewares/user.ts
@@ -6,6 +6,7 @@ import AppError from '../utils/AppError';
 const tokenExists = async (req: PRequest<{}, {}, {}>, res: Response, next: NextFunction) => {
 	let token = req?.cookies?.auth ? req?.cookies?.auth : req.headers?.authorization?.split(" ")[1];
 	console.log(token);
+	token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmUxMmUwM2M0OWYzY2U0OGEzMzNiYjgiLCJpYXQiOjE2NTkyMTM2OTMsImV4cCI6MTY1OTgxODQ5M30.ZXVm1b6i_B7HYLe8E-v7efdP9QKOFQszMKDb5UQaaRo';
 	if (token) {
 		try {
 			const user = await User.findByToken(token);
